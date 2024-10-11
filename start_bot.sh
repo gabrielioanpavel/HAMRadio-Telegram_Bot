aa110b3f2a45dddbea5f040c115dc1d91a57f974
gabrielioanpavel <pavelgabrielioan@gmail.com>
2024-10-11 21:01:50 +0300

    Fixed start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PATH_TO_BOT=$(find / -type d -name 'Bot_POTA-Telegram' 2> /dev/null -print -quit)
+PATH_TO_BOT=$(find / -type d -name 'HAMRadio-Telegram_Bot' 2> /dev/null -print -quit)
 PATH_TO_BOT+="/src/bot.py"
 python3 $PATH_TO_BOT
