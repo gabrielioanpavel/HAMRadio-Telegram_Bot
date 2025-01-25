71b333cb3f9885d98132c469ebce3e76e91e44e4
Gabriel Pavel <pavelgabrielioan@gmail.com>
2025-01-25 17:35:41 +0200

    Update start_bot.sh
    Signed-off-by: Gabriel Pavel <pavelgabrielioan@gmail.com>

--- start_bot.sh
+++ start_bot.sh
@@ -3,8 +3,4 @@
 PATH_TO_BOT=$(find / -type d -name 'HAMRadio-Telegram_Bot' 2> /dev/null -print >
 PATH_TO_BOT+="/src/bot.py"
 
-PATH_TO_SIGNAL_CHECKER=$(find / -type d -name 'HAMRadio-Telegram_Bot' 2> /dev/n>
-PATH_TO_SIGNAL_CHECKER+="/src/check_for_signal.py"
-
-python3 $PATH_TO_BOT &
-python3 $PATH_TO_SIGNAL_CHECKER
+python3 $PATH_TO_BOT
